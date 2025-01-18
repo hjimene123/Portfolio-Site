94a451c43aa72dd2fd3d95e1c958bbb71fa757fb
hjimene123 <143016157+hjimene123@users.noreply.github.com>
2025-01-17 21:49:33 -0500

    Removed parallax from script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,3 @@ $('#toggle').click(function() {
    $('#overlay').toggleClass('open');
   });
 
-// JavaScript for parallax effect
-window.addEventListener('scroll', function () {
-    const parallax = document.querySelector('.parallax');
-    const scrollPosition = window.pageYOffset;
-    parallax.style.backgroundPosition = 'center ' + (scrollPosition * 0.5) + 'px';
-});
