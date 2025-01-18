8f88fbdc7597748e7be6ae7de5dce5e3c3bf85f3
hjimene123 <143016157+hjimene123@users.noreply.github.com>
2025-01-17 21:37:55 -0500

    script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+// JavaScript for parallax effect
+window.addEventListener('scroll', function () {
+    const parallax = document.querySelector('.parallax');
+    const scrollPosition = window.pageYOffset;
+    parallax.style.backgroundPosition = 'center ' + (scrollPosition * 0.5) + 'px';
+});
